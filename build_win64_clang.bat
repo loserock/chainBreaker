ceba7ed2b9ad5a72572ffc3132440cd2bf0caad0
Péter HANDBAUER <peter.handbauer@nng.com>
2017-09-25 14:29:49 +0200

    fix build script on win64

--- build_win64_clang.bat
+++ build_win64_clang.bat
@@ -1,4 +1,4 @@
 md build
 md build\win64
-clang++ --std=c++14 -Wall -pthread -O3 -Iinclude -Llib\x64 -v -lSDL2main -lSDL2 -o build\win64\chainBreaker.exe main.cpp render_window.cpp
+clang++ -std=c++14 -Wall -pthread -O3 -Iinclude -Llib\x64 -v -Wl,-subsystem:console -lSDL2main -lSDL2 -o build\win64\chainBreaker.exe main.cpp render_window.cpp
 copy lib\x64\SDL2.dll build\win64\SDL2.dll
