43784968d7cdb7b33e95f68993d2c7cd13ace62f
Péter HANDBAUER <peter.handbauer@nng.com>
2017-09-29 17:18:05 +0200

    change default window to fullscreen

--- render_window.cpp
+++ render_window.cpp
@@ -8,7 +8,7 @@ void RenderWindow::InitWindow()
 {
     ASSERT(SDL_Init(SDL_INIT_VIDEO) < 0);
     auto &w = m_windowShape;
-    m_window = SDL_CreateWindow(m_windowTitle.c_str(), w.x, w.y, w.width, w.height, SDL_WINDOW_HIDDEN);
+    m_window = SDL_CreateWindow(m_windowTitle.c_str(), w.x, w.y, w.width, w.height, SDL_WINDOW_MAXIMIZED /*TODO SDL_WINDOW_HIDDEN*/);
     m_surface = SDL_GetWindowSurface(m_window);
     
     // TODO
